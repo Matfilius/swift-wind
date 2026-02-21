e58e41481719d6c387a9e48c199cf6f2a04f41e9
nesos242 <lamesicnebojsa@gmail.com>
2026-02-21 17:58:05 +0100

    Update PlayerController.cs

--- Fast Wind/Assets/C# Scripts/Player/PlayerController.cs
+++ Fast Wind/Assets/C# Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,20 +6,44 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player Component References")]
     [SerializeField] Rigidbody2D rb;
+    Animator animator;
 
     [Header("Player Settings")]
     [SerializeField] float speed;
     [SerializeField] float jumpingPower;
+    float horizontalInput;
+    bool isFacingRight = true;
+
 
     [Header("Grounding")]
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform groundCheck;
 
     private float horizontal;
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        Flip();
+
+    }
+
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        animator.SetFloat("xVelocity", Math.Abs(rb.linearVelocity.x));
+    }
+
+    void Flip()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+
+        }
     }
 
     #region PLAYER MOVEMENT
