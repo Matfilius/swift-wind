6f5c2ebfbe06fbd9f7e85ef44a29cfa635535133
petar2008 <petarpucar908@gmail.com>
2026-06-04 19:35:46 +0200

    skoror zavrseno treba nam jos scena

---
+++ Fast Wind/Assets/C# Scripts/ArchitectureScripts/SceneLoadTrigger.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private string[] scenesToLoad;
+    [SerializeField] private string[] scenesToUnload;
+
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            LoadScenes();
+            UnloadScenes();
+        }
+    }
+
+    private void LoadScenes()
+    {
+        for(int i=0; i<scenesToLoad.Length; i++)
+        {
+            bool isSceneLoaded = false;
+
+            for (int j = 0; j < SceneManager.sceneCount; j++)
+            {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if(loadedScene.name == scenesToLoad[i])
+                {
+                    isSceneLoaded = true;
+                    break;
+                }
+            }
+
+            if (!isSceneLoaded)
+            {
+                SceneManager.LoadSceneAsync(scenesToLoad[i], LoadSceneMode.Additive);
+            }
+        }
+    }
+
+    private void UnloadScenes()
+    {
+        for(int i = 0; i < scenesToUnload.Length; i++)
+        {
+            for(int j = 0; j < SceneManager.sceneCount; j++)
+            {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if(loadedScene.name == scenesToUnload[i])
+                {
+                    SceneManager.UnloadSceneAsync(scenesToUnload[i]);
+                }
+            }
+        }
+    }
+
+}
